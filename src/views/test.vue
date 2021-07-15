fa439dbe347e10197b6c5037f912fe11d03b94af
하동 <ha90111@naver.com>
2021-07-15 22:33:01 +0900

    practice vue

---
+++ src/views/test.vue
@@ -0,0 +1,37 @@
+{
+	// Place your snippets for vue here. Each snippet is defined under a snippet name and has a prefix, body and 
+	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
+	// same ids are connected.
+	// Example:
+	"init template": {
+		"prefix": "vue-start",
+		"body": [
+      "<template >",
+      "<div>",
+      "</div>",
+      "</template>",
+      "<script>",
+      "export default {",
+      "name: '',",
+      "components: {},",
+      "data(){",
+      "\treturn{",
+      "\t\tsampleData:''",
+      "\t};",
+      "},",
+      "setup() {},",
+      "created() {},",
+      "mounted() {},",
+      "unmounted() {},",
+      "methods: {}",
+      "}",
+      "</script>",
+      "<style>",
+      "",
+      "</style>",
+      ""
+		],
+		"description": "Generate Basic Vue Code"
+	}
+}
